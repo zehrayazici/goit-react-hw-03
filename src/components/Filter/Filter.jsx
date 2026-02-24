2407662ea6c0c5748767051fb7d0590fb9507f2c
zy <zehrayazici1998@outlook.com>
2026-02-24 16:13:22 +0300

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -3,14 +3,17 @@ import styles from './Filter.module.css'
 export default function Filter({ value, onChange }) {
   return (
     <div className={styles.wrapper}>
+      {}
       <input
         type="text"
+        id="filter-search-input"
+        name="search"
         value={value}
         onChange={onChange}
-        name="search"
         autoComplete="off"
         placeholder="search.."
         className={styles.input}
+        aria-label="Search filter" 
       />
     </div>
   )
